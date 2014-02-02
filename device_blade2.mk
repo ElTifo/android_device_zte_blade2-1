409b69fdf9fc21318b73cf41b5ece7a7e04e853f
ElTifo <tifo.el@gmail.com>
2014-02-02 19:02:02 +0000

    Update device_blade2.mk

--- device_blade2.mk
+++ device_blade2.mk
@@ -3,13 +3,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/zte/blade2/blade2-vendor.mk)
+$(call inherit-product-if-exists, vendor/zte/blade2_s/blade2-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/zte/blade2/overlay
+DEVICE_PACKAGE_OVERLAYS += device/zte/blade2_s/overlay
 
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/zte/blade2/recovery_kernel
+	LOCAL_KERNEL := device/zte/blade2_s/recovery_kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
@@ -23,6 +23,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := generic_blade2
 PRODUCT_DEVICE := blade2
 
-pathmap_INCL := recovery:device/zte/blade2/recovery
+pathmap_INCL := recovery:device/zte/blade2_s/recovery
 
 
